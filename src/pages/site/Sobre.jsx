8941147d2dfbaf58599865121a78f2b1292ff3a0
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-06 13:54:34 +0100

    Página sobre feito

--- src/pages/site/Sobre.jsx
+++ src/pages/site/Sobre.jsx
@@ -1,11 +1,17 @@
 import BannerSobre from "../../components/sobre/BannerSobre";
+import Institucional from "../../components/sobre/Institucional";
+import MissaoVisaoValores from "../../components/sobre/MissaoVisaoValores";
+import TimelineEmpresa from "../../components/sobre/TimelineEmpresa";
 
 export default function Sobre() {
   return (
     <>
       <title>Sobre | Imobiliária</title>
 
-      <Banner/>
+      <BannerSobre />
+      <Institucional />
+      <MissaoVisaoValores />
+      <TimelineEmpresa />
     </>
   );
 }
