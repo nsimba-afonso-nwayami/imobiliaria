4dcfbe2ed95e526e1bf1b92800adf82b852444f4
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-06 15:38:44 +0100

    Página de imóveis criada e feita

---
+++ src/pages/site/Imoveis.jsx
@@ -0,0 +1,11 @@
+import Imoveiss from "../../components/imoveis/Imoveiss";
+
+export default function Imoveis() {
+  return (
+    <>
+      <title>Imóveis | Imobiliária</title>
+
+      <Imoveiss />
+    </>
+  );
+}
