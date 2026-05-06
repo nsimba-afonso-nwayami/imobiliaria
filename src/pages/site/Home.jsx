75b4932575012becd745b4ea5f9df7a6c3c4bbdf
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-06 12:39:53 +0100

    Página home feita

--- src/pages/site/Home.jsx
+++ src/pages/site/Home.jsx
@@ -1,4 +1,12 @@
 import Hero from "../../components/home/Hero";
+import BuscaPropriedade from "../../components/home/BuscaPropriedade";
+import ImoveisDestaque from "../../components/home/ImoveisDestaque";
+import Sobre from "../../components/home/Sobre";
+import NossosDiferenciais from "../../components/home/NossosDiferenciais";
+import ImoveisCategoria from "../../components/home/ImoveisCategoria";
+import Depoimentos from "../../components/home/Depoimentos";
+import OutrosImoveis from "../../components/home/OutrosImoveis";
+import Cta from "../../components/home/Cta";
 
 export default function Home() {
   return (
@@ -6,6 +14,14 @@ export default function Home() {
       <title>Imobiliária</title>
 
       <Hero />
+      <BuscaPropriedade />
+      <ImoveisDestaque />
+      <Sobre />
+      <NossosDiferenciais />
+      <ImoveisCategoria />
+      <Depoimentos />
+      <OutrosImoveis />
+      <Cta />
     </>
   );
 }
