e8681c987e74d2eedaf5b8d828051ecf2732d5f8
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-06 12:41:52 +0100

    Página sobre criada

---
+++ src/pages/site/Sobre.jsx
@@ -0,0 +1,11 @@
+import BannerSobre from "../../components/sobre/BannerSobre";
+
+export default function Sobre() {
+  return (
+    <>
+      <title>Sobre | Imobiliária</title>
+
+      <Banner/>
+    </>
+  );
+}
