975b42db91f3503a9bb6fdf122717324a044fcd5
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-07 09:59:39 +0100

    Primeiro commit comprador

---
+++ src/pages/comprador/DashboardComprador.jsx
@@ -0,0 +1,15 @@
+import CompradorLayout from "./components/CompradorLayout";
+import Modal from "./components/Modal";
+
+export default function DashboardComprador() {
+
+  return (
+    <>
+      <title>Dashboard Comprador | Imobiliária</title>
+
+      <CompradorLayout title="Início">
+        
+      </CompradorLayout>
+    </>
+  );
+}
