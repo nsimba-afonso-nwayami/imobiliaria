343763638c221b494617378092379237f9ca7b3e
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-21 15:41:55 +0100

    Proxy configurado

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://imobiliaria.hossidev.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
+
