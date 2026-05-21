2f54e6e5e02d45b37759d40c3b35afa286498c2f
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-21 16:16:36 +0100

    Token automático adicionado

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-//const API_URL = "http://imobiliaria.hossidev.com/api/";
+const API_URL = "http://imobiliaria.hossidev.com/api/";
 //const API_URL = "/api/";
 
 export const api = axios.create({
@@ -8,4 +8,15 @@ export const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+// TOKEN AUTOMÁTICO
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
