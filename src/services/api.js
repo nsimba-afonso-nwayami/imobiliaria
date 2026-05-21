2f9dcf149f4c6701d3c60fbe9b96ea2dd65d9d2d
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-21 16:39:56 +0100

    Usando proxy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://imobiliaria.hossidev.com/api/";
-//const API_URL = "/api/";
+//const API_URL = "http://imobiliaria.hossidev.com/api/";
+const API_URL = "/api/";
 
 export const api = axios.create({
   baseURL: API_URL,
